16863bcd13e3c08f78afbe48bddac70d869d9b89
Miguel Ángel Alarcos Torrecillas <miguel.alarcos@gmail.com>
2018-09-20 15:05:52 +0200

    second commit; new repository

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 import { SDP_Mixin, connect } from './sdp'
 import { moduleSocket } from './socketModule'
 
-//exports.SDP_Mixin = SDP_Mixin
-//exports.moduleSocket = moduleSocket
 export { SDP_Mixin, moduleSocket, connect }
